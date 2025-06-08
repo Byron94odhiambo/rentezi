b0f9e28f1e829e6a412bfa5ffc35df80e3e0374d
Hybrid_01 <odhiambobyron39@gmail.com>
2025-06-08 20:59:02 +0300

    app/models/:Initialize models

---
+++ app/models/init.py
@@ -0,0 +1,15 @@
+from .user import User
+from .property import Property
+from .unit import Unit
+from .assignment import Assignment
+from .payment import Payment
+from .maintenance import MaintenanceRequest
+
+__all__ = [
+    "User",
+    "Property", 
+    "Unit",
+    "Assignment",
+    "Payment",
+    "MaintenanceRequest"
+]
